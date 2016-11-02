7c6950e8a6134bb9fc4296427e7ba24dcb2ec39a
Nicolas-DoyonLesarge at codea-scm <Nicolas-DoyonLesarge@codea-scm.aws.mapote.com>
2016-11-02 14:50:56 +0000

    init

---
+++ tabs/Main.lua
@@ -0,0 +1,28 @@
+-- Company Logo
+
+-- Created by: Nicolas Doyon Lesarge
+-- Created On: Oct -2016
+-- Created for: ICS2O
+-- This program displays the company logo
+
+-- Use this function to perform your initial setup
+function setup()
+    supportedOrientations(LANDSCAPE_ANY)
+    displayMode(FULLSCREEN)
+    noFill()
+    noSmooth()
+    pushStyle()
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    -- Do your drawing here
+    sprite("Project:companyLogoImage", WIDTH/2, HEIGHT/2, WIDTH, HEIGHT)
+end
+
